102a0c3cfd4d3db4d5fd13803b312b0f74c0f6ce
Janka Uryga <lolzatu2@gmail.com>
2024-08-15 20:18:25 +0200

    add edge-rendered page that uses %5F

---
+++ src/app/%5Funderscored/page.tsx
@@ -0,0 +1,10 @@
+export const dynamic = "force-dynamic";
+export const runtime = "edge";
+
+export default async function Underscored() {
+  return (
+    <>
+      <div>Hello Underscored!</div>
+    </>
+  );
+}
